ccaaf29b15314c0f989aa45fe1eff28bebe68ff6
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-04-07 21:47:10 +0200

    created SpringBoot project and connected Mongo Atlas database

---
+++ src/main/java/com/homeappliancesshop/HomeAppliancesShopApplication.java
@@ -0,0 +1,13 @@
+package com.homeappliancesshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAppliancesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAppliancesShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeappliancesshop/HomeappliancesshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeappliancesshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeappliancesshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

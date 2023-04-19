1494c9e82678b61a7ac04d1204179de83792bbb6
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-04-19 20:36:37 +0200

    added product model and category model

---
+++ Backend/src/main/java/com/homeappliancesshop/model/Category.java
@@ -0,0 +1,18 @@
+package com.homeappliancesshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    private String categoryId;
+    private String name;
+}

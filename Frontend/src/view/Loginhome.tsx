e032ca5babe10e328b3fdab55bb7ddab81911972
Bartosz <bartoszk00@o2.pl>
2023-04-21 23:32:06 +0200

    implement searching

--- Frontend/src/view/Loginhome.tsx
+++ Frontend/src/view/Loginhome.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import LinkMaterial from '@mui/material/Link';
 import { Link } from "react-router-dom";
 import Paper from '@mui/material/Paper';
@@ -8,8 +7,6 @@ import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Topbar from './../topbar/Topbar';
 import Product from './../view/Product';
-import { styled } from '@mui/material/styles';
-import { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Grid, List, ListItem, ListItemText } from '@mui/material';
 
@@ -28,19 +25,10 @@ const categories = [
   },
 ];
 
-const SearchField = styled(TextField)({
-  marginLeft: 'auto',
-});
-
 function Loginhome() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [activeCategory, setActiveCategory] = useState('');
-  const handleSearch = () => {
-    // przekieruj użytkownika na stronę z wynikami wyszukiwania
-    console.log(`Szukaj: ${searchQuery}`);
-  };
+  const [activeCategory, setActiveCategory] = React.useState('');
 
-    return (
+  return (
       <div>
         <Topbar />
         <Grid container spacing={2} sx={{ height: '100vh' }}>
@@ -49,46 +37,34 @@ function Loginhome() {
               <Typography variant="h6">Kategorie</Typography>
               <List sx={{ marginTop: '16px' }}>
                 {categories.map((category) => (
-                  <div key={category.name}>
-                    <ListItem button onClick={() => setActiveCategory(category.name)}>
-                      <ListItemText primary={category.name} />
-                    </ListItem>
-                    <List sx={{ marginTop: '8px' }}>
-                      {activeCategory === category.name &&
-                        category.subcategories.map((subcategory) => (
-                          <ListItem
-                            key={subcategory}
-                            button
-                            component={Link}
-                            to={`/Loginhome?subcategory=${subcategory}`}
-                          >
-                            <ListItemText primary={subcategory} />
-                          </ListItem>
-                        ))}
-                    </List>
-                  </div>
+                    <div key={category.name}>
+                      <ListItem button onClick={() => setActiveCategory(category.name)}>
+                        <ListItemText primary={category.name} />
+                      </ListItem>
+                      <List sx={{ marginTop: '8px' }}>
+                        {activeCategory === category.name &&
+                            category.subcategories.map((subcategory) => (
+                                <ListItem
+                                    key={subcategory}
+                                    button
+                                    component={Link}
+                                    to={`/Loginhome?subcategory=${subcategory}`}
+                                >
+                                  <ListItemText primary={subcategory} />
+                                </ListItem>
+                            ))}
+                      </List>
+                    </div>
                 ))}
               </List>
             </Paper>
           </Grid>
           <Grid item xs={12} md={10} sx={{marginTop: '5px'}}>
-            <Grid container justifyContent='center'>
-              <Grid item xs={12} md={6} >
-                <SearchField
-                  fullWidth
-                  label="Wyszukaj produkt"
-                  variant="standard"
-                  value={searchQuery}
-                  onChange={(event) => setSearchQuery(event.target.value)}
-                />
-              </Grid>
-            </Grid>
-                 <Product />
+            <Product />
           </Grid>
         </Grid>
       </div>
-    );
-  }
+  );
+}
 
 export default Loginhome;
-
--- Frontend/src/view/Product.tsx
+++ Frontend/src/view/Product.tsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import axios from 'axios';
+import Box from '@mui/material/Box';
 
 interface Product {
     productId: string;
     name: string;
+    brand: string;
+    color: string;
+    specification: string;
+    price: number;
     category: {
         categoryId: string;
         name: string;
     };
 }
+
 let url = 'http://localhost:8080';
+
 function Products() {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
@@ -39,11 +47,14 @@ function Products() {
         </div>
     );
 }
+
 interface ProductContentProps {
     category: string;
 }
+
 function ProductContent({ category }: ProductContentProps) {
     const [products, setProducts] = useState<Product[]>([]);
+    const [searchText, setSearchText] = useState('');
 
     useEffect(() => {
         getProducts();
@@ -60,19 +71,41 @@ function ProductContent({ category }: ProductContentProps) {
             });
     };
 
-    const filteredProducts = products.filter(product => product.category.categoryId === category);
+    const filteredProducts = products.filter((product) => {
+        const productString = `${product.name} ${product.brand} ${product.color} ${product.specification}`.toLowerCase();
+        return product.category.categoryId === category && productString.includes(searchText.toLowerCase());
+    });
 
     return (
         <>
-            {filteredProducts.length > 0 ? (
-                <ul>
-                    {filteredProducts.map((product) => (
-                        <li key={product.productId}>{`Name: ${product.name ?? 'unknown'}`}</li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Loading products...</p>
-            )}
+            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+                <TextField
+                    label="Szukaj"
+                    variant="outlined"
+                    value={searchText}
+                    onChange={(event) => setSearchText(event.target.value)}
+                    sx={{ mb: 2, width: '50%' }}
+                />
+            </Box>
+                {filteredProducts.length > 0 ? (
+                    <Box sx={{ border: '1px solid grey', padding: '17px', borderRadius: '8px' }}>
+                        <ul style={{ listStyle: 'none', margin: 0, padding: 0 }}>
+                            {filteredProducts.map((product) => (
+                                <li key={product.productId}>
+                                    <Typography>Nazwa: {product.name ?? 'unknown'}</Typography>
+                                    <Typography>Marka: {product.brand ?? 'unknown'}</Typography>
+                                    <Typography>Kolor: {product.color ?? 'unknown'}</Typography>
+                                    <Typography>Specyfikacja: {product.specification ?? 'unknown'}</Typography>
+                                    <Typography>Cena: {product.price ?? 'unknown'}</Typography>
+                                </li>
+                            ))}
+                        </ul>
+                    </Box>
+                ) : (
+                    <p></p>
+                )}
+
+
         </>
     );
 }

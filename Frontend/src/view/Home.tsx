ff61a3849458e7de1375bfaae320968998501a57
Bartosz <bartoszk00@o2.pl>
2023-05-05 17:11:55 +0200

    improvement of the home screen

--- Frontend/src/view/Home.tsx
+++ Frontend/src/view/Home.tsx
@@ -1,17 +1,19 @@
+import Notloggedintopbar from './../topbar/Notloggedintopbar';
 import * as React from 'react';
-import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import LinkMaterial from '@mui/material/Link';
 import { Link } from "react-router-dom";
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import Notloggedintopbar from './../topbar/Notloggedintopbar';
-import { styled } from '@mui/material/styles';
-import { useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Product from './../view/Product';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Grid, List, ListItem, ListItemText } from '@mui/material';
 
 const categories = [
   {
-    name: 'Kategoria 1',
-    subcategories: ['Podkategoria 1', 'Podkategoria 2', 'Podkategoria 3'],
+    name: 'Komputery i tablety',
+    subcategories: ['Laptopy', 'Komputery', 'Podzespoły komputerowe'],
   },
   {
     name: 'Kategoria 2',
@@ -23,19 +25,10 @@ const categories = [
   },
 ];
 
-const SearchField = styled(TextField)({
-  marginLeft: 'auto',
-});
+function Loginhome() {
+  const [activeCategory, setActiveCategory] = React.useState('');
 
-function Home() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [activeCategory, setActiveCategory] = useState('');
-  const handleSearch = () => {
-    // przekieruj użytkownika na stronę z wynikami wyszukiwania
-    console.log(`Szukaj: ${searchQuery}`);
-  };
-
-    return (
+  return (
       <div>
         <Notloggedintopbar />
         <Grid container spacing={2} sx={{ height: '100vh' }}>
@@ -44,45 +37,36 @@ function Home() {
               <Typography variant="h6">Kategorie</Typography>
               <List sx={{ marginTop: '16px' }}>
                 {categories.map((category) => (
-                  <div key={category.name}>
-                    <ListItem button onClick={() => setActiveCategory(category.name)}>
-                      <ListItemText primary={category.name} />
-                    </ListItem>
-                    <List sx={{ marginTop: '8px' }}>
-                      {activeCategory === category.name &&
-                        category.subcategories.map((subcategory) => (
-                          <ListItem
-                            key={subcategory}
-                            button
-                            component={Link}
-                            to={`/category/${category.name}/${subcategory}`}
-                          >
-                            <ListItemText primary={subcategory} />
-                          </ListItem>
-                        ))}
-                    </List>
-                  </div>
+                    <div key={category.name}>
+                      <ListItem button onClick={() => setActiveCategory(category.name)}>
+                        <ListItemText primary={category.name} />
+                      </ListItem>
+                      <List sx={{ marginTop: '8px' }}>
+                        {activeCategory === category.name &&
+                            category.subcategories.map((subcategory) => (
+                                <ListItem
+                                    key={subcategory}
+                                    button
+                                    component={Link}
+                                    to={`/Home?subcategory=${subcategory}`}
+                                >
+                                  <ListItemText primary={subcategory} />
+                                </ListItem>
+                            ))}
+                      </List>
+                    </div>
                 ))}
               </List>
             </Paper>
           </Grid>
           <Grid item xs={12} md={10} sx={{marginTop: '5px'}}>
-            <Grid container justifyContent='center'>
-              <Grid item xs={12} md={6} >
-                <SearchField
-                  fullWidth
-                  label="Wyszukaj produkt"
-                  variant="standard"
-                  value={searchQuery}
-                  onChange={(event) => setSearchQuery(event.target.value)}
-                />
-              </Grid>
-            </Grid>
+            <Product />
           </Grid>
         </Grid>
       </div>
-    );
-  }
+  );
+}
 
-export default Home;
+export default Loginhome;
+
 

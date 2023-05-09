b1337e88b81c3a0b1b1236d1097dc24581de7dfc
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-05-09 19:43:44 +0200

    incrementing and decrementing amount of products in cart

--- Frontend/src/view/ShoppingCart.tsx
+++ Frontend/src/view/ShoppingCart.tsx
@@ -3,46 +3,68 @@ import Topbar from '../topbar/Topbar';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../redux/store';
 import { Button, Box } from '@mui/material';
-import { increment, decrement } from '../redux/ShoppingCartReducer';
+import { decrementAmountOfProduct, incrementAmountOfProduct } from '../redux/ShoppingCartReducer';
 
 
 
+interface Product {
+  productId: string;
+  name: string;
+  brand: string;
+  color: string;
+  specification: string;
+  price: number;
+  imageURL: string;
+  category: {
+    categoryId: string;
+    name: string;
+  };
+}
 
 function Koszyk() {
 
   const shoppingCart = useSelector((state: RootState) => state.shoppingCart);
   const dispatch = useDispatch();
 
+  const incrementAmount = (product: Product) => {
+    dispatch(incrementAmountOfProduct(product));
+  }
+
+  const decrementAmount = (product: Product) => {
+    dispatch(decrementAmountOfProduct(product))
+  }
+
   return (
     <>
       <Topbar />
 
-      <Typography variant="h4" align="center" sx={{ mt: 2 }}>
-        Koszyk: ilosc to: {shoppingCart.quantity}
-      </Typography>
-  
+      {shoppingCart.cart.length > 0 ? (
+        shoppingCart.cart.map((cartElement) => (
+          <li key={cartElement.productDetails.productId}>
+            <Typography>Nazwa: {cartElement.productDetails.name ?? 'unknown'}</Typography>
+            <Typography>Marka: {cartElement.productDetails.brand ?? 'unknown'}</Typography>
+            <Typography>Kolor: {cartElement.productDetails.color ?? 'unknown'}</Typography>
+            <Typography>Specyfikacja: {cartElement.productDetails.specification ?? 'unknown'}</Typography>
+            <Typography>Cena: {cartElement.productDetails.price ?? 'unknown'}</Typography>
+            <Typography>Ilosc: {cartElement.quantity}</Typography>
 
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => incrementAmount(cartElement.productDetails)}>+</Button>
 
-      <Button  onClick={() => dispatch(increment())}> Increment </Button>
-      <Button  onClick={() => dispatch(decrement())}> Decrement </Button>
-
-
-      {shoppingCart.products.length > 0? (
-        shoppingCart.products.map((product) => (
-          <li key={product.productId}>
-            <Typography>Nazwa: {product.name ?? 'unknown'}</Typography>
-            <Typography>Marka: {product.brand ?? 'unknown'}</Typography>
-            <Typography>Kolor: {product.color ?? 'unknown'}</Typography>
-            <Typography>Specyfikacja: {product.specification ?? 'unknown'}</Typography>
-            <Typography>Cena: {product.price ?? 'unknown'}</Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => decrementAmount(cartElement.productDetails)}>-</Button>
           </li>
         ))
 
       ) : (
         <Typography>Koszyk jest pusty</Typography>
       )}
-      
-      
+
+
     </>
   );
 }

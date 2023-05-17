c587e591fd945094c9dce9a6a4a6a7adeebfafb2
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-05-17 18:10:03 +0200

    prepared backend for Stripe

---
+++ Backend/src/main/java/com/homeappliancesshop/model/ChargeRequest.java
@@ -0,0 +1,16 @@
+package com.homeappliancesshop.model;
+
+import lombok.Data;
+
+@Data
+public class ChargeRequest {
+
+    public enum Currency {
+        EUR, USD;
+    }
+    private String description;
+    private int amount;
+    private Currency currency;
+    private String stripeEmail;
+    private String stripeToken;
+}
---
+++ Backend/src/main/java/com/homeappliancesshop/service/StripeService.java
@@ -0,0 +1,32 @@
+package com.homeappliancesshop.service;
+
+import com.homeappliancesshop.model.ChargeRequest;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class StripeService {
+
+    @Value("${STRIPE_SECRET_KEY}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+    public Charge charge(ChargeRequest chargeRequest) throws StripeException {
+        Map<String, Object> chargeParams = new HashMap<>();
+        chargeParams.put("amount", chargeRequest.getAmount());
+        chargeParams.put("currency", chargeRequest.getCurrency());
+        chargeParams.put("description", chargeRequest.getDescription());
+        chargeParams.put("source", chargeRequest.getStripeToken());
+        return Charge.create(chargeParams);
+    }
+}

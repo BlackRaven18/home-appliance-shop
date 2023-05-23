f08e80947616c379048677d39f8ac492fc3b72d9
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-05-23 17:33:26 +0200

    displaying user transaction history

--- Frontend/src/view/History.tsx
+++ Frontend/src/view/History.tsx
@@ -1,22 +1,29 @@
 
-import Typography from '@mui/material/Typography';
+import { Typography, Box, Divider, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import TopBar from '../TopBar/TopBar';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import PriceFormatter from '../PriceFormattingUtils/PriceFormatter';
 
-interface HistoryI{
+interface HistoryI {
     transactionId: string,
+    transactions: TransactionI[],
+}
+
+interface TransactionI {
+    date: string,
+    status: string,
     totalAmount: number,
     deliveryMethod: string,
-    transactions: [
-        date: string,
-        status: string,
-        products: [
-            productId: string,
-            price: number,
-            quantity: number,
-        ]
-    ]
+    products: ProductsInTransactionI[],
+}
+
+interface ProductsInTransactionI {
+    productId: string,
+    price: number,
+    quantity: number,
+    name: string,
 }
 
 function History() {
@@ -25,19 +32,19 @@ function History() {
 
     useEffect(() => {
         getHistory();
-    },[]);
+    }, []);
 
     const getHistory = async () => {
 
-        axios
-        .get(process.env.REACT_APP_BACKEND_URL + "/persons" 
-            + "/64679522e57752643a41b1dc" + "/paymenthistory")
-        .then((response) => {
-            console.log(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+        await axios
+            .get(process.env.REACT_APP_BACKEND_URL + "/persons"
+                + "/64679522e57752643a41b1dc" + "/paymenthistory")
+            .then((response) => {
+                setHistory(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
 
     }
 
@@ -48,9 +55,57 @@ function History() {
                 Historia
             </Typography>
 
-            
 
-        
+            {history ? (
+                history.transactions.map((transaction) => (
+                    <Box
+                        sx={{
+                            border: '1px solid grey',
+                            padding: '15px',
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                            marginBottom: '15px',
+                            borderRadius: '8px',
+                            width: '90%',
+                        }}
+                    >
+                        <Typography>Data: {transaction.date}</Typography>
+                        <Typography>Status: {transaction.status}</Typography>
+                        <Typography>Metoda dostawy: {transaction.deliveryMethod}</Typography>
+                        <Typography>Kwota zamówienia: {transaction.totalAmount}</Typography>
+                        <Typography
+                            variant='h5'
+                            margin="10px"
+                        >
+                            Produkty w zamówieniu
+                        </Typography>
+                        {transaction.products.map((product) => (
+                            <Box>
+                                <Accordion>
+                                    <AccordionSummary
+                                        expandIcon={<ExpandMoreIcon />}
+                                        aria-controls="panel1a-content"
+                                        id="panel1a-header"
+                                    >
+                                        <Typography>Produkt {product.productId}</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                        <Typography>Id produktu: {product.productId}</Typography>
+                                        <Typography>Cena: {PriceFormatter.getFormattedPrice(product.price)}</Typography>
+                                        <Typography>Ilosc: {product.quantity}</Typography>
+                                    </AccordionDetails>
+                                </Accordion>
+                            </Box>
+                        ))}
+
+                    </Box>
+                ))
+            ) : (
+                <p></p>
+            )}
+
+
+
         </div>
     );
 }

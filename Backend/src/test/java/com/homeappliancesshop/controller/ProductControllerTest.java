8b35d95f54d916ffff591a6c84d432f36759e595
fiZZowsky <piotrekr53@interia.pl>
2023-06-05 21:30:24 +0200

    added admin controller tests

--- Backend/src/test/java/com/homeappliancesshop/controller/ProductControllerTest.java
+++ Backend/src/test/java/com/homeappliancesshop/controller/ProductControllerTest.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.*;
 
 class ProductControllerTest {

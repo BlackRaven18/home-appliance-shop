88867e067f30248a4cd0013272035ea34e4f9cde
fiZZowsky <piotrekr53@interia.pl>
2023-06-09 21:44:53 +0200

    added stripe payment gateway controller test

---
+++ Backend/src/test/java/com/homeappliancesshop/controller/StripePaymentGatewayControllerTest.java
@@ -0,0 +1,58 @@
+package com.homeappliancesshop.controller;
+
+import com.homeappliancesshop.dto.ChargeStatusDTO;
+import com.homeappliancesshop.dto.OrderDetailsDTO;
+import com.homeappliancesshop.dto.ProductDetailsDTO;
+import com.homeappliancesshop.service.StripeClientService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+class StripePaymentGatewayControllerTest {
+    @InjectMocks
+    private StripePaymentGatewayController stripePaymentGatewayController;
+
+    @Mock
+    private StripeClientService stripeClientService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        stripePaymentGatewayController = new StripePaymentGatewayController(stripeClientService);
+    }
+
+    @AfterEach
+    void clear() { reset(stripeClientService); }
+
+    @Test
+    public void chargeCard() {
+        String token = "test-token";
+        OrderDetailsDTO orderDetailsDTO = new OrderDetailsDTO();
+        orderDetailsDTO.setBuyerId("test-buyer");
+        orderDetailsDTO.setDeliveryMethod("test-delivery");
+        ProductDetailsDTO product1 = new ProductDetailsDTO();
+        product1.setProductId("p1");
+        product1.setQuantity(2);
+        ProductDetailsDTO product2 = new ProductDetailsDTO();
+        product2.setProductId("p2");
+        product2.setQuantity(1);
+        orderDetailsDTO.setProductDetailsDTO(new ProductDetailsDTO[]{product1, product2});
+
+        ChargeStatusDTO expectedChargeStatus = new ChargeStatusDTO();
+        expectedChargeStatus.setStatus("success");
+        expectedChargeStatus.setMessage("Payment charged successfully");
+
+        when(stripeClientService.chargeNewCard(token, orderDetailsDTO)).thenReturn(expectedChargeStatus);
+
+        ChargeStatusDTO response = stripePaymentGatewayController.chargeCard(orderDetailsDTO, token);
+
+        assertEquals(expectedChargeStatus, response);
+    }
+}

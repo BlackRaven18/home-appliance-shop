c555674c47fb5ee34d2919bb30ef40d501c29445
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-06-13 15:02:13 +0200

    refactor: protecting routes

--- Frontend/src/router/ProtectedElement.tsx
+++ Frontend/src/router/ProtectedElement.tsx
@@ -4,12 +4,19 @@ import UserDataManager from "../UserDataManager/UserDataManager";
 interface ProtectedElementProps {
   element: JSX.Element;
   redirectPath: string;
+  allowLoggedInUser: boolean;
 }
 
-const ProtectedElement = ({element, redirectPath}: ProtectedElementProps): JSX.Element => {
+const ProtectedElement = (
+  { element, redirectPath, allowLoggedInUser }: ProtectedElementProps): JSX.Element => {
+
   const isAuthenticated = UserDataManager.getUserId() ? true : false;
 
-  return isAuthenticated ? element : <Navigate to={redirectPath} />
+  if (allowLoggedInUser) {
+    return isAuthenticated ? element : <Navigate to={redirectPath} />
+  } else {
+    return isAuthenticated? <Navigate to={redirectPath}/> : element;
+  }
 }
 
 export default ProtectedElement;

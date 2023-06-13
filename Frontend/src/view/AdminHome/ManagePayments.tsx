93cdb7e709a8312b5a56b0fbc15a20d6aa9fd2b4
Bartosz <bartoszk00@o2.pl>
2023-06-13 15:34:20 +0200

    displaying transaction history

--- Frontend/src/view/AdminHome/ManagePayments.tsx
+++ Frontend/src/view/AdminHome/ManagePayments.tsx
@@ -1,8 +1,125 @@
-import Typography from '@mui/material/Typography';
+import { Button, TextField } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import PriceFormatter from '../../PriceFormattingUtils/PriceFormatter';
+import UserDataManager from '../../UserDataManager/UserDataManager';
 
-const ManagePayments = () => {
-    return(
-        <Typography>Zarzadzaj płatnościami</Typography>
+interface HistoryI {
+    transactionId: string,
+    transactions: TransactionI[],
+}
+
+interface TransactionI {
+    date: string,
+    status: string,
+    totalAmount: number,
+    deliveryMethod: string,
+    products: ProductsInTransactionI[],
+}
+
+interface ProductsInTransactionI {
+    productId: string,
+    price: number,
+    quantity: number,
+    name: string,
+    imageURL: string,
+}
+
+function ManagePayments() {
+
+    const [history, setHistory] = useState<HistoryI>();
+    const userId = UserDataManager.getUserId();
+    const [selectedTransactionId, setSelectedTransactionId] = useState<string | null>(null);
+
+    useEffect(() => {
+        getHistory();
+    }, []);
+
+    const getHistory = async () => {
+
+        await axios
+            .get(process.env.REACT_APP_BACKEND_URL + "/persons/"
+                + userId + "/paymenthistory", {
+                auth: {
+                    username: UserDataManager.getUsername(),
+                    password: UserDataManager.getPassword()
+                }
+            })
+            .then((response) => {
+                console.log("response in history:" + response.data)
+                setHistory(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+    }
+
+    return (
+        <div>
+            {history ? (
+                history.transactions.map((transaction) => (
+                    <Box
+                        sx={{
+                            border: '1px solid grey',
+                            padding: '15px',
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                            marginBottom: '15px',
+                            borderRadius: '8px',
+                            width: '90%',
+                        }}
+                    >
+                        <Typography>Data: {transaction.date}</Typography>
+                        <Typography>Status: {transaction.status}</Typography>
+                        <Typography>Metoda dostawy: {transaction.deliveryMethod}</Typography>
+                        <Typography>Kwota zamówienia: {PriceFormatter.getFormattedPrice(transaction.totalAmount)}</Typography>
+                        <Typography
+                            variant='h5'
+                            margin="10px"
+                        >
+                            Produkty w zamówieniu
+                        </Typography>
+                        {transaction.products.map((product) => (
+                            <Box>
+                                <Accordion>
+                                    <AccordionSummary
+                                        expandIcon={<ExpandMoreIcon />}
+                                        aria-controls="panel1a-content"
+                                        id="panel1a-header"
+                                    >
+                                        <Typography>Produkt {product.name}</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                        <Typography>Id produktu: {product.productId}</Typography>
+                                        <Typography>Cena: {PriceFormatter.getFormattedPrice(product.price)}</Typography>
+                                        <Typography>Ilosc: {product.quantity}</Typography>
+                                        <Typography>Obrazek: {product.imageURL}</Typography>
+                                    </AccordionDetails>
+                                </Accordion>
+                            </Box>
+                        ))}
+                        {transaction.status === 'failed' && (
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                style={{ marginTop: '10px' }}
+                            >
+                                Zatwierdź
+                            </Button>
+                        )}
+                    </Box>
+                ))
+            ) : (
+                <p></p>
+            )}
+
+
+
+        </div>
     );
 }
+
 export default ManagePayments;

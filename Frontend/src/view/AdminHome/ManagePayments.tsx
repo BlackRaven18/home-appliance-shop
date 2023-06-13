689b7710e24d92c0ebcf8ff70046a44875dfda94
Bartosz <bartoszk00@o2.pl>
2023-06-13 21:28:31 +0200

    problem with payments

--- Frontend/src/view/AdminHome/ManagePayments.tsx
+++ Frontend/src/view/AdminHome/ManagePayments.tsx
@@ -4,33 +4,63 @@ import axios from 'axios';
 import PersonInterface from "../shared/PersonInterface";
 import { Button, TextField, Box } from "@mui/material";
 import Grid from "@mui/material/Grid";
+import UserDataManager from "../../UserDataManager/UserDataManager";
 
 interface TransactionInterface {
-    date: string;
-    status: string;
+    transactionId: string;
+    transactions: string[];
 }
 
 interface ExtendedPersonInterface extends PersonInterface {
     personId: string;
-    transactions_history: TransactionInterface[];
+    transactions_history: TransactionInterface;
 }
 
 const ManagePayments = () => {
     const [people, setPeople] = useState<ExtendedPersonInterface[]>([]);
     const [searchTerm, setSearchTerm] = useState("");
     const [currentPerson, setCurrentPerson] = useState<ExtendedPersonInterface | null>(null);
-    const [approvedTransactions, setApprovedTransactions] = useState<string[]>([]);
+    const [approvedTransactions, setApprovedTransactions] = useState<string>();
 
 
     useEffect(() => {
         getUsers();
+        getTrans("64679522e57752643a41b1dc");
     }, []);
 
     const getUsers = () => {
         axios
-            .get(`${process.env.REACT_APP_BACKEND_URL}/persons`)
+            .get(`${process.env.REACT_APP_BACKEND_URL}/persons`,{
+                auth: {
+                    username: UserDataManager.getUsername(),
+                    password: UserDataManager.getPassword()
+                }
+            })
             .then((response) => {
                 setPeople(response.data);
+
+                people.forEach((element) => {
+                    const transId = element.transactions_history;
+                    //getTrans(transId);
+                    console.log(transId.transactionId);
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+
+    const getTrans = ( id : string) => {
+        axios
+            .get(`${process.env.REACT_APP_BACKEND_URL}/persons/`+id+`/paymenthistory`,{
+                auth: {
+                    username: UserDataManager.getUsername(),
+                    password: UserDataManager.getPassword()
+                }
+            })
+            .then((response) => {
+                setApprovedTransactions(response.data);
+                //console.log(response.data);
             })
             .catch(function (error) {
                 console.log(error);
@@ -48,19 +78,23 @@ const ManagePayments = () => {
 
     const handleTransactionApproval = (personId: string, date: string) => {
         // Znajdź osobę na podstawie personId
-        const person = people.find((person) => person.personId === personId);
+        //const person = people.find((person) => person.personId === personId);
 
+        const person = {};
+        console.log(person);
         if (person) {
             // Znajdź transakcję w historii transakcji osoby na podstawie daty
-            const transaction = person.transactions_history.find((transaction) => transaction.date === date);
+            //console.log(person.transactions_history);
 
-            if (transaction) {
+            //const transaction = person.transactions_history.find((transaction) => transaction.date === date);
+
+            /*if (transaction) {
                 // Zaktualizuj status transakcji na "manually-accepted"
                 transaction.status = 'manually-accepted';
 
                 // Dodaj transakcję do zatwierdzonych transakcji
                 setApprovedTransactions([...approvedTransactions, `${personId}_${date}`]);
-            }
+            }*/
         }
     };
 
@@ -90,29 +124,29 @@ const ManagePayments = () => {
                                     <p style={{ fontSize: '20px' }}><strong>Nazwisko:</strong> {person ? person.surname : 'unknown'}</p>
                                     <p style={{ fontSize: '20px' }}><strong>Email:</strong> {person ? person.email : 'unknown'}</p>
                                     <p style={{ fontSize: '20px' }}><strong>Numer telefonu:</strong> {person ? person.phoneNumber : 'unknown'}</p>
-                                    {person.transactions_history && person.transactions_history.length > 0 ? (
-                                        person.transactions_history.map((transaction, transactionIndex) => (
-                                            <div key={transactionIndex} style={{ marginTop: '10px' }}>
-                                                <p style={{ fontSize: '16px' }}><strong>Data transakcji:</strong> {transaction.date}</p>
-                                                <p style={{ fontSize: '16px' }}><strong>Status transakcji:</strong> {transaction.status}</p>
-                                                {transaction.status === 'failed' && (
-                                                    <Button
-                                                        variant="contained"
-                                                        color="primary"
-                                                        size="small"
-                                                        onClick={() => handleTransactionApproval(person.personId, transaction.date)}
-                                                        disabled={approvedTransactions.includes(`${person.personId}_${transaction.date}`)}
-                                                    >
-                                                        Zatwierdź
-                                                    </Button>
+                                    {/*{person.transactions_history ? (*/}
+                                    {/*    person.transactions_history.map((transaction, transactionIndex) => (*/}
+                                    {/*        <div key={transactionIndex} style={{ marginTop: '10px' }}>*/}
+                                    {/*            <p style={{ fontSize: '16px' }}><strong>Data transakcji:</strong> {transaction.date}</p>*/}
+                                    {/*            <p style={{ fontSize: '16px' }}><strong>Status transakcji:</strong> {transaction.status}</p>*/}
+                                    {/*            {transaction.status === 'failed' && (*/}
+                                    {/*                <Button*/}
+                                    {/*                    variant="contained"*/}
+                                    {/*                    color="primary"*/}
+                                    {/*                    size="small"*/}
+                                    {/*                    onClick={() => handleTransactionApproval(person.personId, transaction.date)}*/}
+                                    {/*                    disabled={approvedTransactions.includes(`${person.personId}_${transaction.date}`)}*/}
+                                    {/*                >*/}
+                                    {/*                    Zatwierdź*/}
+                                    {/*                </Button>*/}
 
-                                                )
-                                                }
-                                            </div>
-                                        ))
-                                    ) : (
-                                        <p>Brak historii transakcji dla tej osoby.</p>
-                                    )}
+                                    {/*            )*/}
+                                    {/*            }*/}
+                                    {/*        </div>*/}
+                                    {/*    ))*/}
+                                    {/*) : (*/}
+                                    {/*    <p>Brak historii transakcji dla tej osoby.</p>*/}
+                                    {/*)}*/}
                                 </div>
                             ))
                         )}
